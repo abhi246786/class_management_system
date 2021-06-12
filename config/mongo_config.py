d6acb2b7aab2be10a61470b5ebf56769cddc8083
Abhishek Singh <75206165+abhi246786@users.noreply.github.com>
2021-06-12 14:01:46 +0530

    Add files via upload

---
+++ config/mongo_config.py
@@ -0,0 +1,24 @@
+from flask_pymongo import PyMongo
+import os
+
+MONGO_DBNAME = 'classadmin'
+MONGO_URI = 'mongodb://localhost:27017/classadmin'
+MONGO_USERNAME = ''
+MONGO_PASSWORD = ''
+
+# The global connection object which will be shared
+mongo = None
+
+def setup_mongo_connection(app):
+	global mongo
+
+	MONGO_URL = os.environ.get('MONGO_URL')
+	if not MONGO_URL:
+		MONGO_URL = MONGO_URI
+
+	app.config['MONGO_DBNAME'] = MONGO_DBNAME
+	app.config['MONGO_URI'] = MONGO_URL
+	app.config['MONGO_USERNAME'] = MONGO_USERNAME
+	app.config['MONGO_PASSWORD'] = MONGO_PASSWORD
+
+	mongo = PyMongo(app)
